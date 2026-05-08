f203571a5ebbc8863241f256e21e6ade4a7e8d3f
ajaykavalkar-bacancy <ajay.kavalkar@bacancy.com>
2026-05-08 20:41:53 +0530

    change apiUrl for backend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-api-app.vercel.app/api'  // your backend Vercel URL
+  apiUrl: 'https://stream-csv-project.vercel.app/api' // your backend Vercel URL
+//   apiUrl: 'https://your-api-app.vercel.app/api'  // your backend Vercel URL
 };
